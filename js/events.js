b1ed6f948e9091d4a6a498998e2542f7ecac4812
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-19 16:46:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,7 +5,13 @@ function getIt(){
     });
 }
 
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  });
+}
 $(document).ready(function(){
 // call functions here
   getIt();
+  frameIt();
 });
